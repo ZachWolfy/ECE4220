f864f9a161d4e97816c7297a41d16abb81419969
Wan Zach Wan Nawawi <31355218+ZachWolfy@users.noreply.github.com>
2020-10-01 13:38:54 -0500

    Add files via upload

--- one_thread.c
+++ one_thread.c
@@ -5,6 +5,7 @@
 #include <time.h>
 //============================================================================================================================================================
 //part 1-1
+//struct to pass values to the function thread
 struct Convo{
     int **arr_before;
     int **arr_after;
@@ -13,56 +14,62 @@ struct Convo{
     int num_convo;
 };
 //============================================================================================================================================================
+//convolution function for threads
 void *Convolution(void *item){
 
     struct Convo *struct_item = (struct Convo*)item;
-
+	//traverse whole matrix
     for(int i = 0; i < (*struct_item).row; i++)
     {
         for(int j = 0; j < (*struct_item).col; j++)
         {
+		//if first column ignore the value before if not enter if
             	if(j != 0)
 		{
 			(*struct_item).arr_after[i][j] += ((*struct_item).arr_before[i][j-1])*((*struct_item).mask[0]);
 		}
+		//if last column, ignore the value after if not enter if
 		if(j != (*struct_item).col-1)
 		{
 			(*struct_item).arr_after[i][j] += ((*struct_item).arr_before[i][j+1])*((*struct_item).mask[2]);
 		}
+		//middle element value
 		(*struct_item).arr_after[i][j] += ((*struct_item).arr_before[i][j])*((*struct_item).mask[1]);
-		(*struct_item).num_convo++;
+		(*struct_item).num_convo++; // increase number of convolution
         }
     }
 
     return NULL;
 }
 //============================================================================================================================================================
+//checking function to check if code is run properly by stating name of file
 int Check(int argc, char *argv[])
 {
 	if(argc > 2)
 	{
-		printf("Error too many arguments\n");
+		printf("Error too many arguments \n(how to run: ./one_thread MxN.txt \n");
 		return 0;
 	}
 	else if(argc < 2)
 	{
-		printf("Error expected one more argument\n");
+		printf("Error expected one more argument \n(how to run: ./one_thread MxN.txt \n"");
 		return 0;
 	}
     	return 1;//if everything checks well return 1 as in true
 }
 //============================================================================================================================================================
+//main function
 int main(int argc, char *argv[])
 {
     	//read file
 	int check = Check(argc, argv);//Check() function
 	
-	//if Check function return 0, exit program
+	// Check function return 0, exit program
 	if(check == 0)
 	{
 		return 0;
 	}
-
+	//open file
     	char *p = (char*)malloc(sizeof(char)*1);//saves file name in heap
     	p = argv[1];
     	printf("Opening %s...\n", p);//print we are opening file number given
@@ -72,7 +79,7 @@ int main(int argc, char *argv[])
 //----------------------------------------------------------------------------------------------------------------------------------
 	FILE *openfile = fopen(p, "r");
 
-
+	//if fail to open file
 	if(openfile == NULL)
 	{
 		printf("File does not exist. Please use a proper data file and include it in the same directory as the program.\n");
@@ -80,11 +87,12 @@ int main(int argc, char *argv[])
 		free(p);
 		return 0;
     	}
+	//success in opening file
 	else
 	{
 		int i,j;
-		fscanf(openfile, "%d", &row);
-		fscanf(openfile, "%d", &col);
+		fscanf(openfile, "%d", &row); //get row
+		fscanf(openfile, "%d", &col); //get column
 		
 
 		//initialize array for both original matrix and container to store convolutioned matrix
@@ -123,14 +131,18 @@ int main(int argc, char *argv[])
     	(*convo).col = col;
     	pthread_t thread;
 
+	//start time
 	clock_t begin = clock();
+
     	//create thread
     	pthread_create(&thread, NULL, Convolution, (void *)convo);
 
-	
+	//join thread
     	pthread_join(thread, NULL);
-    	clock_t end = clock();
+    	//end clock
+	clock_t end = clock();
     	
+	//print convoluted result, time and number of convolutions
     	printf("Convoluted Array\n");
     	for(int m = 0; m < row; m++)
 	{
@@ -143,5 +155,6 @@ int main(int argc, char *argv[])
 	double total_time = (double)(end-begin)/ CLOCKS_PER_SEC;
 	printf("Total time: %lf seconds\n", total_time);
 	printf("Total number of convolution operations performed: %d\n", (*convo).num_convo);
+
 	return 0;
 }
