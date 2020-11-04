8b2d8b42f3d9566bfa4fb64b46f33f03a06d841d
Wan Zach Wan Nawawi <31355218+ZachWolfy@users.noreply.github.com>
2020-11-03 21:36:28 -0600

    Add files via upload

--- lab4_p1.c
+++ lab4_p1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <wiringPi.h>
 #include <fcntl.h>
 #include <pthread.h>
 #include <sys/mman.h>
@@ -7,94 +8,77 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sempahore.h>
 
-struct print
-{
-	struct timeval x1, x2, xbp;
-	int y1, y2, ybp;
-};
+char buffer[2];//gps buffer
+struct timeval x;//timestamp buffer
 
-char buffer[2];
-struct timeval x;
-int numb1;
-
-void *ReadBPE()
+//read the button timestamp pipe received
+//thread to wait for buffer and get the interpolation
+//print GPS data's and timestamp
+void *ReadBPE(void* ptr)
 {
 	//open pipe "/tmp/BP_pipe"
 	int np = open("/tmp/BP_pipe", O_RDONLY);
 	int bpread;
-	struct timeval button_buffer;
+	struct timeval xbp;
+	xbp.tv_sec = 0;
+	xbp.tv_usec = 0;
+	struct timeval x1, x2;
+	double y1, y2, ybp;
+	
 
 	while(1)
 	{
 		//read from "/tmp/BP_pipe"
-		bpread = read(np, &button_buffer, sizeof(struct timeval));
-		pthread_t child;
-		pthread_create(&child, NULL, ChildThread, &button_buffer);
+		bpread = read(np, &xbp, sizeof(struct timeval));
+		
+		//get previous gps
+		y1 = buffer[0];
+		x1 = x;
+		
+		//wait until global buffer is updated.
+		while(y1 == buffer[0])
+		{}
+		
+		//when global is updated
+		y2 = buffer[0];
+		x2 = x;
+		
+		//the difference of struct timeval sec and usec that is converted to milli sec for second gps data timestamp, x2 and first gps data timestamp, x1.
+		int millisec = (x2.tv_sec - x1.tv_sec) + ((int)x2.tv_usec - (int)x1.tv_usec)/1000000;
+		
+		//the difference of struct timeval second and microsecond that is converted to milli sec for button press timestamp, xbp and first gps data timestamp, x1.
+		int millisec2 = (xbp.tv_sec - x1.tv_sec)+ ((int)xbp.tv_usec - (int)x1.tv_usec)/1000000;
+		
+		//interpolation
+		ybp = (((y2-y1)/(millisec))*(millisec2))+y1;
+		
+		//print
+		printf("x1: %d:%d, y1: %lf\n", x1.tv_sec, x1.tv_usec, y1);
+		printf("x2: %d:%d, y2: %lf\n", x2.tv_sec, x1.tv_usec, y2);
+		printf("xbp: %d:%d, ybp: %lf\n\n\n", xbp.tv_sec, xbp.tv_usec, ybp);
 	}
 }
-void *ChildThread(void *ptr)
-{
-	int numb2;
-	numb2 = numb1;
-	
-	struct print data;
-	struct data.xbp = (struct timeval*)ptr;
-	
-	//get previous gps
-	data.y1 = buffer[0];
-	data.x1 = x;
-
-	//wait until global buffer is updated.
-	while(numb2 == numb1){}
-
-	//when global is updated
-	data.y2 = y;
-	data.x2 = x;
-
-	//interpolation
-	data.ybp = (((data.y2-data.y1)/(data.x2-data.x1))*(data.xbp-data.x1))+data.y1;
-	
-	int pd = open("/tmp/Print_pipe", O_WRONLY);
-	pthread_t printth;
-	pthread_create(&printth, NULL, PrintFunction, NULL);
-	write(pd, &data, sizeof(struct print));
-}
-
-void *PrintFunction()
-{
-	struct print data;
-	
-	int pd = open("/tmp/Print_pipe", O_RDONLY);
-	read(pd, &data, size of(struct print));
-	
-	sem_wait(&mySem);
-	printf("xbp: %d, ybp: %d", data.xbp, data.ybp);
-	printf("x1: %d, y1: %d", data.x1, data.y1);
-	printf("x2: %d, y2: %d", data.x2, data.y2);
-	sem_post(&mySem);
-}
-
+//main function to read gps data and get the timestamp of the gps data and store in global buffer
 int main()
 {
-	//open pipe for gps device
-	int nd = open("/tmp/N_pipe1", O_RDONLY);
-  	//open pipe for print pipe
-  	int pd = open("/tmp/P_pipe", O_RDONLY);
-  
+	//open pipe
+	int np = open("/tmp/N_pipe1", O_RDONLY);
+	int numb;
+	
 	//create thread read bpe
 	pthread_t readbpe;
 	pthread_create(&readbpe, NULL, ReadBPE, NULL);
-
+	
 	//while loop
 	while(1)
 	{
 		//read from pipe 
-		numb1 = read(np, buffer, sizeof(int));
+		numb = read(np, buffer, sizeof(char));
+		buffer[1] = '\0';
 		
 		//get the time stamp and save in global buffer
-		y = buffer[0];
 		gettimeofday(&x, NULL);
 	}
 }
+
