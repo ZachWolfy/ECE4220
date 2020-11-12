629dc0c1e861414faf508d69d0afa58f7232d5d0
Wan Zach Wan Nawawi <31355218+ZachWolfy@users.noreply.github.com>
2020-11-11 22:42:10 -0600

    Add files via upload

--- server_udp_broadcast_w2.c
+++ server_udp_broadcast_w2.c
@@ -1,8 +1,3 @@
-/* 	Name       : 	server_udp_broadcast.c
-	Author     : 	Luis A. Rivera
-	Description: 	Simple server (broadcast)
-					ECE4220/7220		*/
-
 #define _GNU_SOURCE     /* To get defns of NI_MAXSERV and NI_MAXHOST */
 #include <stdio.h>
 #include <stdlib.h>
@@ -105,10 +100,11 @@ int main(int argc, char *argv[])
                printf("getnameinfo() failed: %s\n", gai_strerror(s));
                exit(EXIT_FAILURE);
            }
-			
+			//if IP similar to first four digits break from loop
 			if(strncmp(myIP, "192", 3) == 0)
 			{
 				printf("\t\taddress: <%s>\n", myIP);
+				break;
 			}
        }
    }
@@ -131,6 +127,7 @@ int main(int argc, char *argv[])
 		//if not don't send anyhthing
 		if(strncmp(buffer, WHOIS, 5) == 0)
 		{
+			//if I am master create and send the message that I am the master
 			if(master == 1)
 			{
 				strncpy(mygenerator, "Wan on ", 7);
@@ -142,6 +139,7 @@ int main(int argc, char *argv[])
 					error("sendto WHOIS master");
 				
 			}
+			//if I am not the master send blank
 			else
 			{
 				n = sendto(sock, "\n", 35, 0,
@@ -159,6 +157,7 @@ int main(int argc, char *argv[])
 			
 			//copy # ip
 			strncpy(mygenerator, "# ", 2);
+			mygenerator[2] = '\0';
 			strcat(mygenerator, myIP);
 			strcat(mygenerator, " ");
 			mygenerator[16] = '\0';
@@ -179,12 +178,27 @@ int main(int argc, char *argv[])
 		//compare if haven't lose to see who is the master
 		else if(strncmp(buffer, CHECK, 1) == 0 && lose == 0)
 		{
-	
-			//get random number from sender
-			strcpy(copy, buffer+16);
+			//count white space to get sender number of digits in IP address 
+			int count_white_space = 0;
+			int i;
+			
+			//loop to find white space and count how many characters
+			//in the string and at most 2 whitespace
+			for(i = 0; count_white_space < 2; i++)
+			{
+				//if found whitespace increment by 1 
+				if(buffer[i] == ' ')
+				{
+					count_white_space++;
+				}
+			}
+			
+			//copy random number from sender
+			strcpy(copy, buffer+i);
+			
 			sscanf(copy, "%d", &rcvrand);
 						
-			//if receiver has higher generated number then I lose
+			//if sender has higher generated number then I lose
 			if(rcvrand > random)
 			{
 				lose = 1;
@@ -195,27 +209,40 @@ int main(int argc, char *argv[])
 			{
 				master = 1;
 			}
-			//if random number generated is the same then the person with
-			//the highest IP wins
-			else
-			{printf("enter same\n");
-				strncpy(copy, buffer+2, 13);
-				result = strncmp(myIP, copy, 13);
+			//if random number generated is the same then the sender
+			else if(rcvrand == random)
+			{
+				//number of character in the string -2 to remove count of pound "#"
+				//and whitespace " "
+				i = i - 2;
+				
+				//clear string and copy just the ip address by offset the starting
+				//point of copy by 2 and number of digits with dot "." in the IP
+				memset(copy, 0, sizeof(copy));
+				strncpy(copy, buffer+2, i);
+				
+				//get result value from string compare
+				result = strncmp(myIP, copy, i);
+				//if result is more than 0 that means I have won and I am the master
 				if(result > 0)
-				{printf("enter win same\n");
+				{
 					master = 1;
 				}
-				else
-				{printf("enter lose same\n");
+				//if result is less than 0 that means I have lost and reset master flag
+				//and change lose flag to 1
+				else if(result < 0)
+				{
 					master = 0;
 					lose = 1;
 				}
 			}
+			//send something so client does not stop waiting.
 			n = sendto(sock, "\n", 32, 0,
     		      (struct sockaddr *)&addr, fromlen);
 			if (n  < 0)
 				error("sendto");
 		}
+		//if sender sends other than "WHOIS", "VOTE", "# IP RANDOM_NUMBER"
 		else
 		{
 			n = sendto(sock, "Got a message. Was it from you?\n", 32, 0,
@@ -223,18 +250,18 @@ int main(int argc, char *argv[])
 			if (n  < 0)
 				error("sendto");
 		}
+		//clear inside these string
 		memset(mygenerator, 0, sizeof(mygenerator));
+		memset(copy, 0, sizeof(copy));
 		memset(random_s, 0, sizeof(random_s));
+		memset(buffer, 0, sizeof(buffer));
+		
        // To send a broadcast message, we need to change IP address to broadcast address
        // If we don't change it (with the following line of code), the message
        // would be transmitted to the address from which the message was received.
 	   // You may need to change the address below (check ifconfig)
        addr.sin_addr.s_addr = inet_addr("192.168.1.255");		// broadcast address
 
-       /*n = sendto(sock, "Got a message. Was it from you?\n", 32, 0,
-    		      (struct sockaddr *)&addr, fromlen);
-       if (n  < 0)
-    	   error("sendto");*/
    }
 
    return 0;
